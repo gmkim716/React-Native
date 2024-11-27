df17f634079f1204b100bb49cfa77dc3f039d4e0
Gyeongmin Kim <gmkim716@gmail.com>
2024-11-28 01:33:53 +0900

    chapter 5: create modal

--- mobile-gyver/app/(tabs)/index.tsx
+++ mobile-gyver/app/(tabs)/index.tsx
@@ -1,9 +1,15 @@
 import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
+import { type ImageSource } from "expo-image";
 
 import Button from "@/components/Button";
 import ImageViewer from "@/components/ImageViewer";
-import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import IconButton from "@/components/IconButton";
+import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
+import EmojiList from "@/components/EmojiList";
+import EmojiSticker from "@/components/EmojiSticker";
 
 const PlaceholderImage = require("@/assets/images/background-image.png");
 
@@ -11,36 +17,82 @@ export default function Index() {
   const [selectedImage, setSelectedImage] = useState<string | undefined>(
     undefined
   );
+  const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
+  const [pickedEmoji, setPickedEmoji] = useState<ImageSource | undefined>(
+    undefined
+  );
 
   const pickImageAsync = async () => {
-    // launchImageLibraryAsync 함수를 사용하여 이미지 라이브러리를 열 수 있다
+    // launchImageLibrarimageSourceyAsync 함수를 사용하여 이미지 라이브러리를 열 수 있다
     let result = await ImagePicker.launchImageLibraryAsync({
-      mediaTypes: ["images"],
-      allowsEditing: true,
-      aspect: [4, 3],
-      quality: 1,
+      mediaTypes: ["images"], // 이미지만 선택할 수 있도록 설정
+      allowsEditing: true, // 이미지 편집을 허용
+      aspect: [4, 3], // 이미지의 가로 세로 비율을 4:3으로 설정
+      quality: 1, // 이미지 품질을 1로 설정, 0.1 ~ 1 사이의 값을 가질 수 있다
     });
 
     if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);
+      setSelectedImage(result.assets[0].uri); // assets[0]: 선택한 이미지의 정보
+      setShowAppOptions(true); // 이미지 선택 후에는 앱 옵션을 보여준다
     } else {
       alert("You cancelled the image picker.");
     }
   };
 
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
+  };
+
+  const onSaveImageAsync = async () => {};
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
-      </View>
-      <View style={styles.footerContainer}>
-        <Button
-          theme="primary"
-          label="Choose a photo"
-          onPress={pickImageAsync}
+        <ImageViewer
+          imgSource={PlaceholderImage}
+          selectedImage={selectedImage}
         />
-        <Button label="Use this photo" />
+        {pickedEmoji && (
+          <EmojiSticker imageSize={60} stickerSource={pickedEmoji} />
+        )}
       </View>
+      {showAppOptions ? (
+        <View style={styles.optionsContainer}>
+          <View style={styles.optionsRow}>
+            <IconButton icon="refresh" label="Reset" onPress={onReset} />
+            <CircleButton onPress={onAddSticker} />
+            <IconButton
+              icon="save-alt"
+              label="Save"
+              onPress={onSaveImageAsync}
+            />
+          </View>
+        </View>
+      ) : (
+        <View style={styles.footerContainer}>
+          <Button
+            theme="primary"
+            label="Choose a photo"
+            onPress={pickImageAsync}
+          />
+          <Button
+            label="Use this photo"
+            onPress={() => setShowAppOptions(true)}
+          />
+        </View>
+      )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
+      </EmojiPicker>
     </View>
   );
 }
@@ -58,5 +110,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3, // 1/3만큼의 공간을 차지
     alignItems: "center",
+  },
+  optionsContainer: {
+    position: "absolute",
+    bottom: 60,
+  },
+  optionsRow: {
+    alignItems: "center",
+    flexDirection: "row",
   },
 });
---
+++ mobile-gyver/components/EmojiPicker.tsx
@@ -0,0 +1,54 @@
+import { Modal, View, Text, Pressable, StyleSheet } from "react-native";
+import { PropsWithChildren } from "react";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+
+type Props = PropsWithChildren<{
+  isVisible: boolean;
+  onClose: () => void;
+}>;
+
+export default function EmojiPicker({ isVisible, children, onClose }: Props) {
+  return (
+    <Modal
+      animationType="slide" // modal이 화면에서 슬라이드되는 애니메이션: slide, fade, none
+      transparent={true} // modal이 투명하게 보이도록 설정
+      visible={isVisible} // modal이 보이는지 여부
+    >
+      <View style={styles.modalContent}>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Choose a sticker</Text>
+          <Pressable onPress={onClose}>
+            <MaterialIcons name="close" color="#fff" size={22} />
+          </Pressable>
+        </View>
+        {children}
+      </View>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  modalContent: {
+    height: "25%",
+    width: "100%",
+    backgroundColor: "#25292e",
+    borderTopRightRadius: 18,
+    borderTopLeftRadius: 18,
+    position: "absolute",
+    bottom: 0,
+  },
+  titleContainer: {
+    height: "16%",
+    backgroundColor: "#464C55",
+    borderTopRightRadius: 10,
+    borderTopLeftRadius: 10,
+    paddingHorizontal: 20,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    color: "#fff",
+    fontSize: 16,
+  },
+});
